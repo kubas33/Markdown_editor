41cd6856432bed47f0a254276ab26b62be022f74
kubas33 <Stasiak91@gmail.com>
2023-09-08 14:14:39 +0200

    Add nodemon, and move helpers to another file

---
+++ helpers/handlebars-helpers.js
@@ -0,0 +1,17 @@
+const handlebarsHelpers = {
+  formatDate: function (dateTimeString) {
+    const options = {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+    };
+    const dateTime = new Date(dateTimeString);
+    return dateTime.toLocaleDateString("en-GB", options);
+  },
+};
+
+module.exports = {
+  handlebarsHelpers,
+};
