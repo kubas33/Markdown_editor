0442ead28b6852173ba0adc1f8e41a46641fdd3b
kubas33 <Stasiak91@gmail.com>
2023-09-14 11:22:45 +0200

    Implement add new document function

--- controllers/documentController.js
+++ controllers/documentController.js
@@ -2,13 +2,15 @@ const asyncHandler = require('../node_modules/express-async-handler');
 const fs = require('fs').promises;
 const path = require('path');
 const MarkdownIt = require('markdown-it');
-const { createDataDocument } = require('../services/dataService');
+const DataService = require('../services/dataService');
+const Document = require('../models/documentModel');
 
 const md = new MarkdownIt({
   breaks: true,
 });
 
 const DATA_FILE_NAME = path.join(process.cwd(), 'public', 'data', 'data.json');
+const dataService = new DataService(DATA_FILE_NAME);
 
 exports.index = asyncHandler(async (req, res, next) => {
   res.send('NOT IMPLEMENTED: Site Home Page');
@@ -52,14 +54,27 @@ exports.document_create_get = asyncHandler(async (req, res, next) => {
 
 // Handle document create on POST.
 exports.document_create_post = asyncHandler(async (req, res, next) => {
-  const { title, content } = req.body;
-  const userId = 1;
+  try {
+    const { title, content } = req.body;
+    const userId = 1;
+    const now = new Date().toISOString();
+    const id = await dataService.generateDocumentId();
 
-  const newDocument = {
-    title,
-    content,
-    user_id: userId,
-  };
+    const newDocument = new Document(
+      id,
+      title,
+      content,
+      userId,
+      now,
+      now,
+      false
+    );
+    await dataService.createDocument(newDocument);
+    res.redirect('/documents');
+  } catch (error) {
+    console.error('Błąd podczas dodawania nowego dokumentu: ', error);
+    res.status(500).json({ success: false, error: 'Wystąpił błąd serwera' });
+  }
 
   //res.send("NOT IMPLEMENTED: document create POST");
 });
