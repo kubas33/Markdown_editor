6ee3bcb635d66fbe21e26d77247c2520664c04b2
kubas33 <Stasiak91@gmail.com>
2023-09-19 07:13:03 +0200

    Add delete document for show details view

--- public/js/showDocActions.js
+++ public/js/showDocActions.js
@@ -5,8 +5,15 @@ document.addEventListener('DOMContentLoaded', function () {
     const url = `./${id}/update`;
     const content = document.querySelector('#markdown');
     const title = document.querySelector('#title-input');
+    const delBtn = document.querySelector('[data-delete-url]');
+    const confirmDelBtn = document.querySelector('#confirm-delete-modal-btn');
+    const confirmDelForm = document.querySelector('#confirm-delete-form');
+    const confirmDelModal = new bootstrap.Modal(
+    document.getElementById('confirmDelete')
+  );
 
     saveBtn.addEventListener('click', async () => {
+
       try {
         const response = await fetch(url, {
           method: 'PUT',
@@ -33,5 +40,39 @@ document.addEventListener('DOMContentLoaded', function () {
       console.error('Błąd podczas aktualizowania dokumentu:', error);
       alert('Wystąpił błąd podczas aktualizowania dokumentu.');
       }
+
     });
+    
+    delBtn.addEventListener('click', async () => {
+      confirmDelBtn.dataset.deleteUrl = delBtn.dataset.deleteUrl; 
+      confirmDelModal.show();
+    });
+
+    confirmDelForm.addEventListener('submit', async (event) => {
+      event.preventDefault();
+      
+      try {
+      const response = await fetch(confirmDelBtn.dataset.deleteUrl, {
+        method: 'DELETE',
+      });
+      console.log(response);
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log({ data });
+        if (data.success) {
+          window.location.href = '/documents';
+        } else {
+          console.error('Błąd serwera:', response.status);
+          alert('Wystąpił błąd podczas usuwania dokumentu.');
+        }
+      } else {
+        alert('Wystąpił błąd podczas usuwania dokumentu.');
+      }
+    } catch (error) {
+      console.error('Błąd podczas usuwania dokumentu:', error);
+      alert('Wystąpił błąd podczas usuwania dokumentu.');
+    }
+    });
+    confirmDelModal.hide();
 });
